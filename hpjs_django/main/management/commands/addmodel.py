93ca4c5b8446d08271f8485b42315697884d8b8a
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-06-18 22:22:44 +0200

    finished manually adding model to database

--- hpjs_django/main/management/commands/addmodel.py
+++ hpjs_django/main/management/commands/addmodel.py
@@ -11,8 +11,23 @@ class Command(BaseCommand):
 
     self.stdout.write("adding model to database", ending='\n')
 
+    # adding HPJS_Model
     model1 = HPJS_Model(name="Mnist")
     model1.save()
+    
+    # adding trial and linking it to the model
     trial1 = Trial(trial=1, start_time="2019-02-12 12:12", 
       end_time="2019-02-12 12:12", accuracy=0.3, hpjs_model=model1)
+
     trial1.save()
+
+    # adding parameter value and linking it to the model
+    parameter1 = Parameter(parameter_name="modelType", hpjs_model=model1)
+    parameter1.save()
+
+    # adding parameter value and linking it to the parameter and trial
+    parametervalue1 = ParameterValue(value="DenseNet", trial=trial1, 
+      parameter=parameter1)
+
+    parametervalue1.save()
+
