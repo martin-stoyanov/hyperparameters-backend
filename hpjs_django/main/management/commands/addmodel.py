bb4d98fa91c10249df4414a7239bc3ad699d7b2a
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-06-18 20:36:43 +0200

    deleting previous records so we can re-run the admin-command

--- hpjs_django/main/management/commands/addmodel.py
+++ hpjs_django/main/management/commands/addmodel.py
@@ -3,6 +3,12 @@ from main.models import HPJS_Model, Trial, Parameter, ParameterValue
 
 class Command(BaseCommand):
   def handle(self, *args, **options):
+    # deleting previous records so we can re-run the admin-command
+    HPJS_Model.objects.all().delete()
+    Trial.objects.all().delete()
+    Parameter.objects.all().delete()
+    ParameterValue.objects.all().delete()
+
     self.stdout.write("adding model to database", ending='\n')
 
     model1 = HPJS_Model(name="Mnist")
