ac7598c6be6931ec6eb991659154137f9398ff9c
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-06-18 21:56:30 +0200

    admin command works for trial

--- hpjs_django/main/management/commands/addmodel.py
+++ hpjs_django/main/management/commands/addmodel.py
@@ -13,3 +13,6 @@ class Command(BaseCommand):
 
     model1 = HPJS_Model(name="Mnist")
     model1.save()
+    trial1 = Trial(trial=1, start_time="2019-02-12 12:12", 
+      end_time="2019-02-12 12:12", accuracy=0.3, hpjs_model=model1)
+    trial1.save()
