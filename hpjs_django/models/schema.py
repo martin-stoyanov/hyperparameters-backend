c91c56798b24288568896ecb665621e8934f8dfb
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-06-23 20:55:28 +0300

    querying Trial in schema.py

--- hpjs_django/models/schema.py
+++ hpjs_django/models/schema.py
@@ -1,13 +1,21 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from .models import HPJS_Model
+from .models import HPJS_Model, Trial
 
 class ModelType(DjangoObjectType):
   class Meta:
     model = HPJS_Model
 
+class TrialType(DjangoObjectType):
+  class Meta:
+    model = Trial
+
 class ModelQuery(object):
   all_models = graphene.List(ModelType)
+  all_trials = graphene.List(TrialType)
 
   def resolve_all_models(self, info, **kwargs):
     return HPJS_Model.objects.all()
+
+  def resolve_all_trials(self, info, **kwargs):
+    return TrialType.objects.select_related('hpjs_model').all()
